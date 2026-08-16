518135e891309c314dc5e7bbcbff0ef3b9c329ce
Lyxn-git <pepesergu@gmail.com>
2026-08-16 16:11:33 +0200

    fixed cli arguments

--- src/cli.rs
+++ src/cli.rs
@@ -4,17 +4,17 @@ use clap::Parser;
 #[clap(name = "kosmochlor", about = "Continuous Shape and Symmetry Measurements in Rust.", author = "JSG (jose.serranog@ub.edu)", version = env!("CARGO_PKG_VERSION"))]
 pub struct Cli {
     /// Path or name of the .xyz file containing the atom labels and coordinates of the problem shape.
-    name: String,
+    pub name: String,
 
     /// Treat the problem shape as a non-centered polyhedron.
     #[arg(short = 'n', long = "nc")]
-    not_centered: bool,
+    pub not_centered: bool,
 
     /// IDs of the standard reference shapes to compare the problem shape to as found in the SHAPE 2.1 user manual.
     #[arg(short = 's', long = "sh", num_args = 1..)]
-    shapes: Option<Vec<u8>>,
+    pub shapes: Option<Vec<usize>>,
 
     /// Name or path of YAML files containing user-defined reference shapes to compare the problem shape to.
     #[arg(short = 'r', long ="ref", num_args = 1..)]
-    user_shapes: Option<Vec<String>>
+    pub user_shapes: Option<Vec<String>>
 }
