07548395378dc9ed25c25549698999d577644ae5
JSG-UB <pepesergu@gmail.com>
2026-09-03 12:38:33 +0200

    added modules

--- src/csom/mod.rs
+++ src/csom/mod.rs
@@ -3,3 +3,20 @@ mod io;
 mod dev;
 mod tests;
 mod optimize;
+
+
+#[derive(Debug)]
+pub enum CsomError {
+    WrongSpaceGroup { pg: String},
+
+}
+
+impl std::fmt::Display for CsomError  {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            CsomError::WrongSpaceGroup { pg } => {write!(f, "wrong point group name: {}", {pg})}
+        }
+    }
+}
+
+impl std::error::Error for CsomError {}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,1 +1,3 @@
 pub mod standard_shapes;
+pub mod pgs;
+mod symops;
