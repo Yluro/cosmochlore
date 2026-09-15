50a7230a017dd9ccadf9f6abc833a97f825ad42a
JSG-UB <pepesergu@gmail.com>
2026-09-15 16:35:34 +0200

    Added thiserror dependency to not have to implement the std::Error to all modules errors manually.

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use crate::csom::CsomError;
+use crate::odis::OdisError;
+use crate::shapes::ShapeLookupError;
+use crate::xyz::XyzParseError;
+use crate::yaml::YamlParseError;
+
+/// Crate-wide error, covering every module's own error type plus file I/O.
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Xyz(#[from] XyzParseError),
+    #[error(transparent)]
+    Yaml(#[from] YamlParseError),
+    #[error(transparent)]
+    Shape(#[from] ShapeLookupError),
+    #[error(transparent)]
+    Csom(#[from] CsomError),
+    #[error(transparent)]
+    Odis(#[from] OdisError),
+    #[error(transparent)]
+    Io(#[from] std::io::Error),
+}
